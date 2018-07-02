dc52462da810fb441c48ed07c9875d5b896bd56a
Juan Sebastián Gaitán Villamizar <juans.gaitan@gmail.com>
2018-07-02 22:52:50 +0200

    Show all exchanges

--- main.js
+++ main.js
@@ -5,20 +5,23 @@ async function producer(type, consumer, payload) {
   if (type === 0) {
     console.log('Producer ready.');
     return consumer(0, producer);
-  } else if (type === 1) {
+  }
+  if (type === 1) {
     const { orders = [], previous = 0, action = null } = payload;
-    console.log('Last orders:', orders);
+    console.log('Last orders:', orders.slice(-3));
     await wait(1);
     console.log('Producer sends:', previous + 1);
     if (action) {
       console.log('Producer applies action.');
+      const next = await action(previous + 1);
       return consumer(1, producer, {
         acc: orders,
-        current: await action(previous + 1)
+        current: next
       });
     }
     return consumer(1, producer, { acc: orders, current: previous + 1 });
-  } else if (type === 2) {
+  }
+  if (type === 2) {
     return console.log('Producer stops sending.');
   }
 }
@@ -27,23 +30,24 @@ async function consumer(type, producer, payload) {
   if (type === 0) {
     console.log('Consumer ready.');
     return producer(1, consumer, 'Start sending!');
-  } else if (type === 1) {
+  }
+  if (type === 1) {
     const { acc, current } = payload;
     await wait(1);
-    const orders = acc.concat(current).slice(-3);
+    const orders = acc.concat(current);
     console.log(`Consumer received: ${current}`);
     if (current > 19) {
       console.log('Stop sending...');
-      return producer(2);
+      producer(2);
+      return orders;
     } else if (current % 3 === 0) {
       console.log('Consumer requests action...');
       const action = (value) =>
         new Promise((resolve) => {
-          const rand = ~~Math.max(Math.random() * 6000, 3000) + 1000;
-          console.log(`Action: wait for ${rand}ms`);
+          console.log('Action: wait for 3s');
           setTimeout(() => {
             resolve(value);
-          }, rand);
+          }, 3000);
         });
       return producer(1, consumer, {
         orders,
@@ -51,9 +55,11 @@ async function consumer(type, producer, payload) {
         action
       });
     }
-
     return producer(1, consumer, { orders, previous: current });
   }
 }
 
-producer(0, consumer);
+(async () => {
+  const exchanges = await producer(0, consumer);
+  console.log('All exchanges:', exchanges);
+})();
